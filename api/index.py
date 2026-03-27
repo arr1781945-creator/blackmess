3b7f40a348da3d11af41c0553dc7b843d45f6e9a
zerodayhunter375 <arr1781945@gmail.com>
2026-03-27 11:28:37 +0900

    Fix Django settings module path

--- api/index.py
+++ api/index.py
@@ -2,7 +2,7 @@ import os
 import sys
 
 sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'myproject.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
 
 from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
