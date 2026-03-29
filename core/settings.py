23ed78a503445df195d710d5581df9dbf7a73c08
zerodayhunter375 <arr1781945@gmail.com>
2026-03-29 18:43:21 +0900

    Add drf_spectacular to INSTALLED_APPS

--- core/settings.py
+++ core/settings.py
@@ -22,6 +22,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'drf_spectacular',
     'rest_framework_simplejwt',
     'corsheaders',
     'apps.users',
