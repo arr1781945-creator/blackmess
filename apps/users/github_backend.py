09f123e18bf41b92fab8bb860a81523423a5e476
zerodayhunter375 <arr1781945@gmail.com>
2026-03-31 16:35:44 +0900

    fix: override auth_url force https

--- apps/users/github_backend.py
+++ apps/users/github_backend.py
@@ -1,7 +1,16 @@
 from social_core.backends.github import GithubOAuth2
 
+CALLBACK = 'https://black-message-production.up.railway.app/oauth/complete/github/'
+
 class GithubOAuth2HTTPS(GithubOAuth2):
     REDIRECT_STATE = False
-    
+
     def get_redirect_uri(self, state=None):
-        return 'https://black-message-production.up.railway.app/oauth/complete/github/'
+        return CALLBACK
+
+    def auth_url(self):
+        url = super().auth_url()
+        return url.replace(
+            'http://black-message-production.up.railway.app',
+            'https://black-message-production.up.railway.app'
+        )
