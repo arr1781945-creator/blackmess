3721d9a012bc5b12e0ce9e3e2fa9d741880ff576
zerodayhunter375 <arr1781945@gmail.com>
2026-05-04 06:15:46 +0900

    fix: use InMemoryChannelLayer, remove Redis dependency

--- core/settings_full.py
+++ core/settings_full.py
@@ -117,8 +117,7 @@ CACHES = {
 
 CHANNEL_LAYERS = {
     'default': {
-        'BACKEND': 'channels_redis.core.RedisChannelLayer',
-        'CONFIG': {'hosts': [REDIS_URL]},
+        'BACKEND': 'channels.layers.InMemoryChannelLayer',
     }
 }
 
